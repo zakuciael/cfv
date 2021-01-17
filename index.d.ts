1a070eee8daa49ca50f2ae6749ba60f3559d8dd1
Krzysztof Saczuk <zakuciael@outlook.com>
2021-01-17 20:35:23 +0100

    Wrapped pe file logic into one method and added typescript declarations.

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+import { PathLike } from "fs";
+
+export declare const getFileVersion: (path: PathLike) => Promise<string>;
+export declare const getFileVersion: (data: Buffer) => string;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-(() => {
-    console.log("Hello, World!");
-})();
+import { PEFile } from "./lib/PEFile";
+import { PathLike, promises as fs } from "fs";
+
+export const getFileVersion = async (data: Buffer | PathLike): Promise<string> => {
+    if (typeof data === "string") data = await fs.readFile(data);
+
+    const pe = new PEFile(data as Buffer);
+    return [
+        pe.VS_FIXED_FILE_INFO[0].FileVersionMS >> 16,
+        pe.VS_FIXED_FILE_INFO[0].FileVersionMS & 0xffff,
+        pe.VS_FIXED_FILE_INFO[0].FileVersionLS >> 16,
+        pe.VS_FIXED_FILE_INFO[0].FileVersionLS & 0xffff,
+    ].join(".");
+};
